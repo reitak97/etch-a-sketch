937123112060159115827b8d557a8eff166a4599
reitak97 <reita.koyama97@gmail.com>
2024-05-25 16:53:19 -0400

    created board

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    createBoard(16);
+});
+function createBoard(size){
+    let board = document.querySelector('.board');
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let numDivs = size * size;
+    for (let i = 0; i < numDivs; i++){
+        let div = document.createElement('div');
+        div.style.backgroundColor = "yellow";
+        board.insertAdjacentElement('beforeend', div);
+
+    }
+}
